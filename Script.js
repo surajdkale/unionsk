cf896a024ea9e3693a92f563a7a2097f24a721de
surajdkale <67196007+surajdkale@users.noreply.github.com>
2021-01-12 12:51:07 +0000

    Update Script.js

--- Script.js
+++ Script.js
@@ -9,11 +9,12 @@
                	result = document.getElementById("result");
                	year = document.getElementById("year");
 
-           
-                amount.innerHTML = "If your deposit is" + p;
-                rate.innerHTML = "at an interest rate of " + r ;
-                result.innerHTML ="You will recieve an amount of " + (p*n*r/100);
-                year.innerHTML = "in the Year" + n;
+           		
+           				
+           		amount.innerHTML = "If your deposit is<mark>" +p+"</mark>";
+				rate.innerHTML = "at an interest rate of <mark>" + r +"</mark>";
+                result.innerHTML ="You will recieve an amount of <mark>" + (p*n*r/100)+"</mark>";
+                year.innerHTML = "in the Year<mark>" + n+"</mark>";
 			}
 
 	
